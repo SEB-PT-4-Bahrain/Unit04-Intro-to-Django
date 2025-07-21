f0371cd297c9908d3326b4a1ea7f81056b7b6a9f
omarakamal <omaraakamal@gmail.com>
2025-07-21 19:22:44 +0300

    update

--- class-code/fruits-code-along/main_app/views.py
+++ class-code/fruits-code-along/main_app/views.py
@@ -14,9 +14,13 @@ def test_route(request):
 
 def all_fruits(request):
         
-        fruits = Fruit.objects.all()
+        fruits = Fruit.objects.all() #method to get all fruits
         
         return render(request,"all-fruits.html",{"fruits":fruits})
+
+def show_fruit(request,id):
+      fruit = Fruit.objects.get(id =id)
+      return render(request,'show-fruit.html',{fruit})
 
 # set up a route
 
